7f4d473f408fb26b2e23f04f7a21ff4fbb335b82
iceberg717 <mohit717.py@gmail.com>
2026-01-22 02:45:22 +0530

    added c pattern

---
+++ pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, num = 1;
+    printf("enter number:");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", num);
+            num += 1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
